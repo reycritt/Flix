4dead3a8249f8d9be64a378090769948c6dd20b2
Rey <reycrittenden@yahoo.com>
2020-02-09 14:36:40 -0600

    Some tweaks to the views, and the addition of a new detail controller for superheroes

---
+++ Flix/SuperheroViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SuperheroViewController.swift
+//  Flix
+//
+//  Created by cory on 2/9/20.
+//  Copyright © 2020 cory. All rights reserved.
+//
+
+import UIKit
+
+class SuperheroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
